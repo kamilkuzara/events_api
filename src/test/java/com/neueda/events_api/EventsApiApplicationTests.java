ee1abcd76add8183ff42f8552c2f8463ea858a93
Kamil Kuzara <kamilkuzara@outlook.com>
2025-08-18 10:25:48 +0000

    Set up the Spring Boot project

---
+++ src/test/java/com/neueda/events_api/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.events_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
